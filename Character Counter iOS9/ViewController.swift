0de9438f8850a4ab4776f01b8681be1ca61c30ab
tmehta2442 <tmehta@post.harvard.edu>
2016-01-19 22:28:54 -0800

    some styling, arrays, labels

--- Character Counter iOS9/ViewController.swift
+++ Character Counter iOS9/ViewController.swift
@@ -8,8 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var txtName: UITextField!
+    @IBOutlet weak var lblTotalCharacters: UILabel!
+    
+    var nameData = ""
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,41 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func btnCalculate(sender: UIButton) {
+        calculate()
+    }
+    
+    @IBAction func btnClear(sender: UIButton) {
+        clear()
+    }
+    
+    @IBAction func txtNameACTION(sender: UITextField) {
+        calculate()
+    }
+    
+    func calculate(){
+        nameData = txtName.text!
+        printCharacters()
+        hideKeyboard()
+    }
 
+    func clear(){
+        txtName.text = ""
+        lblTotalCharacters.text = "0"
+        hideKeyboard()
+    }
+    
+    func printCharacters(){
+        var nameArray = Array(nameData.characters)
+        lblTotalCharacters.text = "\(nameArray.count)"
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        hideKeyboard()
+    }
+    
+    func hideKeyboard(){
+        txtName.resignFirstResponder()
+    }
 }
 
